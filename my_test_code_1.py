12dbcc6d0c79e8545b72cfc268248ccaaa24b3eb
dc-cxn <d.carr@connexin.com>
2024-03-26 17:47:25 +0000

    added my_code

---
+++ my_test_code_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin python
+
+sf_gw1 = '172.31.255.1/24'
+
+sf_gw2 = input('Enter IP address: ')
+
+print()
+print(f'{"sf_gw1":^20} {"sf_gw2":^20}')
+print(f"{'-' * 20:^20} {'-' * 20:^20}")
+print(f"{sf_gw1:^20} {sf_gw2:^20}")
+print()
